a17577e72b55120e4381c4cf3f8612f5b6a5f5a8
unknown <kanatuulud@gmail.com>
2023-05-03 19:49:54 +0300

    feat: разметка экрана Строки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form}>
+        <Input maxLength={11} isLimitText />
+        <Button text="Развернуть" />
+      </form>
+      <ul className={styles.circle_list}>
+        <Circle />
+      </ul>
     </SolutionLayout>
   );
 };
