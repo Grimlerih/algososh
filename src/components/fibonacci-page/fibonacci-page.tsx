5ff3fd78854f0e9b6dbd7d2d66d0c77a102142d0
unknown <kanatuulud@gmail.com>
2023-05-04 18:12:26 +0300

    feat: функция подсчета fib, FibonacciPage

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import { useState, FC, ChangeEvent, FormEvent } from "react";
 import styles from "./fibonacci-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 
-export const FibonacciPage: React.FC = () => {
+export const FibonacciPage: FC = () => {
+  const [inputValue, setInputValue] = useState<number | string>("");
+  const [loader, setLoader] = useState(false);
+  const [array, setArray] = useState<number[]>([]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const fib = (n: number): number[] => {
+    let arr: number[] = [1, 1];
+    for (let i = 2; i < n + 1; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr;
+  };
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={onSubmit}>
         <Input />
         <Button text="Рассчитать" />
       </form>
       <ul className={styles.circle_list}>
-        <Circle />
+        <Circle />;
       </ul>
     </SolutionLayout>
   );
