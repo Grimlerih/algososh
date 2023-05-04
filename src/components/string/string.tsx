c0af7490a8234fd537e34da16e63aa2da039abd8
unknown <kanatuulud@gmail.com>
2023-05-04 16:44:56 +0300

    feat: функции onChange и onSubmit в StringComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import styles from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
@@ -6,10 +6,25 @@ import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(evt.target.value);
+  };
+
+  const onSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+
   return (
     <SolutionLayout title="Строка">
-      <form className={styles.form}>
-        <Input maxLength={11} isLimitText />
+      <form className={styles.form} onSubmit={onSubmit}>
+        <Input
+          maxLength={11}
+          isLimitText
+          value={inputValue}
+          onChange={onChange}
+        />
         <Button text="Развернуть" />
       </form>
       <ul className={styles.circle_list}>
