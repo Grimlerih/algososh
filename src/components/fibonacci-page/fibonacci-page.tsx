3ddccaa5dbaa4b5bd69ae6b0d9ee9e25e1a0fed0
unknown <kanatuulud@gmail.com>
2023-05-04 17:20:44 +0300

    feat: разметка FibonacciPage

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,7 +4,13 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form className={styles.form} onSubmit={}>
+        <Input />
+        <Button />
+      </form>
+      <ul className={styles.circle_list}>
+        <Circle />;
+      </ul>
     </SolutionLayout>
   );
 };
