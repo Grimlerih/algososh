1ea42c0baa79179ffc1e343a3b19fb0c1c2665e6
unknown <kanatuulud@gmail.com>
2023-05-04 23:20:34 +0300

    feat: логика переключения сортировки SortingPage

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,21 +1,53 @@
-import React from "react";
+import { useState, ChangeEvent, FC } from "react";
 import styles from "./sorting-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Button } from "../ui/button/button";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Column } from "../ui/column/column";
+import { Direction } from "../../types/direction";
 
-export const SortingPage: React.FC = () => {
+export const SortingPage: FC = () => {
+  const [radioValue, setRadioValue] = useState("selectionSort");
+
+  const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setRadioValue(evt.target.value);
+  };
+
+  const handleSort = (order: Direction) => {
+    if (radioValue === "selectionSort") {
+      selectionSort();
+    } else {
+      bubbleSort();
+    }
+  };
+
+  const selectionSort = () => {};
+
+  const bubbleSort = () => {};
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.form}>
         <div className={styles.radioButtons_container}>
-          <RadioInput label="Выбор" />
-          <RadioInput label="Пузырёк" />
+          <RadioInput
+            label="Выбор"
+            value="selectionSort"
+            onChange={onChange}
+            defaultChecked
+          />
+          <RadioInput label="Пузырёк" value="bubbleSort" onChange={onChange} />
         </div>
         <div className={styles.buttons_container}>
-          <Button text="По возрастанию" />
-          <Button text="По убыванию" />
+          <Button
+            text="По возрастанию"
+            sorting={Direction.Ascending}
+            onClick={() => handleSort(Direction.Ascending)}
+          />
+          <Button
+            text="По убыванию"
+            sorting={Direction.Descending}
+            onClick={() => handleSort(Direction.Descending)}
+          />
           <Button text="Новый массив" />
         </div>
       </div>
