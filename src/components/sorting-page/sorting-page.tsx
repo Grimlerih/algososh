84f84cca30245d24c12797eb7443901c16dbb0e3
unknown <kanatuulud@gmail.com>
2023-05-04 22:58:33 +0300

    feat: разметка SortingPage

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { Column } from "../ui/column/column";
 
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={styles.}>
+        <div className={styles.}>
+          <RadioInput label="Выбор" />
+          <RadioInput label="Пузырёк" />
+        </div>
+        <div className={styles.}>
+          <Button text="По возрастанию" />
+          <Button text="По убыванию" />
+          <Button text="Новый массив" />
+        </div>
+      </div>
+      <ul className={styles.}>
+        {array?.map((item) => {
+          return <Column />;
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
