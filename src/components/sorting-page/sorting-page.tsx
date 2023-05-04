dd09572abaf295e2997821a179effe3f096a81aa
unknown <kanatuulud@gmail.com>
2023-05-04 23:05:43 +0300

    style: SortingPage

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./sorting-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Button } from "../ui/button/button";
 import { RadioInput } from "../ui/radio-input/radio-input";
@@ -7,22 +8,18 @@ import { Column } from "../ui/column/column";
 export const SortingPage: React.FC = () => {
   return (
     <SolutionLayout title="Сортировка массива">
-      <div className={styles.}>
-        <div className={styles.}>
+      <div className={styles.form}>
+        <div className={styles.radioButtons_container}>
           <RadioInput label="Выбор" />
           <RadioInput label="Пузырёк" />
         </div>
-        <div className={styles.}>
+        <div className={styles.buttons_container}>
           <Button text="По возрастанию" />
           <Button text="По убыванию" />
           <Button text="Новый массив" />
         </div>
       </div>
-      <ul className={styles.}>
-        {array?.map((item) => {
-          return <Column />;
-        })}
-      </ul>
+      <ul className={styles.column_list}></ul>
     </SolutionLayout>
   );
 };
