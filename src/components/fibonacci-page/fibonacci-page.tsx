fac7e58cf65cd48b1a1861fb491a9db0f4d11cd9
unknown <kanatuulud@gmail.com>
2023-05-04 17:29:51 +0300

    style:  FibonacciPage

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import styles from "./fibonacci-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const FibonacciPage: React.FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-      <form className={styles.form} onSubmit={}>
+      <form className={styles.form}>
         <Input />
-        <Button />
+        <Button text="Рассчитать" />
       </form>
       <ul className={styles.circle_list}>
-        <Circle />;
+        <Circle />
       </ul>
     </SolutionLayout>
   );
