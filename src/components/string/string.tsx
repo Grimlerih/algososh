c31cd55865bd3c20c4f9a742eee882b9512a8c0f
unknown <kanatuulud@gmail.com>
2023-05-04 16:54:27 +0300

    feat: изменение onSubmit в StringComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,9 +4,18 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { DELAY_IN_MS } from "../../constants/delays";
+import { ElementStates } from "../../types/element-states";
+
+interface IString {
+  symbol: string;
+  state: ElementStates;
+}
 
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [array, setArray] = useState<IString[]>([]);
 
   const onChange = (evt: ChangeEvent<HTMLInputElement>) => {
     setInputValue(evt.target.value);
@@ -14,6 +23,24 @@ export const StringComponent: React.FC = () => {
 
   const onSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+    const firstIndex = 0;
+    const secondIndex = inputValue.length - 1;
+
+    const inputArr = Array.from(inputValue);
+    const outputArr: IString[] = [];
+
+    setInputValue("");
+    setIsLoading(true);
+
+    inputArr.forEach((item) => {
+      const value = {
+        symbol: item,
+        state: ElementStates.Default,
+      };
+      outputArr.push(value);
+    });
+
+    setArray([...outputArr]);
   };
 
   return (
