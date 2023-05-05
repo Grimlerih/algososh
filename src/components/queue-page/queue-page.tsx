3404a8678f8dac1f66751f8e403ddccfe361c301
unknown <kanatuulud@gmail.com>
2023-05-05 15:02:10 +0300

    feat: разметка и стили QueuePage

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import styles from "./queue-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { nanoid } from "nanoid";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
-
+      <form className={styles.form}>
+        <div className={styles.input_container}>
+          <Input placeholder="Введите текст" />
+          <Button text={"Добавить"} />
+          <Button text={"Удалить"} />
+        </div>
+        <Button text={"Очистить"} />
+      </form>
+      <ul className={styles.circle_container}>
+        <Circle key={nanoid()} />
+      </ul>
     </SolutionLayout>
   );
 };
