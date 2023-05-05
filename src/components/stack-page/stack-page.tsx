6b8eacc63f00d9737df4aea3a9fd926444799595
unknown <kanatuulud@gmail.com>
2023-05-05 12:32:36 +0300

    feat: разметка SortPage

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { FC } from "react";
+import styles from "./stack-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
-export const StackPage: React.FC = () => {
+export const StackPage: FC = () => {
   return (
     <SolutionLayout title="Стек">
-
+      <form>
+        <div>
+          <Input placeholder="Введите текст" />
+          <Button text={"Добавить"} />
+          <Button text={"Удалить"} />
+        </div>
+        <Button text={"Очистить"} />
+      </form>
+      <ul>
+        <Circle />
+      </ul>
     </SolutionLayout>
   );
 };
