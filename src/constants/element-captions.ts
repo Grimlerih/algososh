ff610857f9f04b71d577b3cbe549d0877c8304a8
Grimlerih <kanatuulud@gmail.com>
2023-06-05 12:50:48 +0300

    test: e2e тест алгоритма разворота строки

--- src/constants/element-captions.ts
+++ src/constants/element-captions.ts
@@ -1,2 +1,8 @@
 export const HEAD = "head";
 export const TAIL = "tail";
+
+export const circle = '[class^="circle_circle"]';
+
+export const DEFAULT_STATE = "4px solid rgb(0, 50, 255)";
+export const CHANGING_STATE = "4px solid rgb(210, 82, 225)";
+export const MODIFIED_STATE = "4px solid rgb(127, 224, 81)";
